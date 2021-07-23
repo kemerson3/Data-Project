a28e360c6b795b1a184b600d67571d5b5648a989
kemerson3 <emersonk@duq.edu>
2021-07-23 14:01:13 -0400

    Updated with Aov and GLM tests

--- Baseline.r
+++ Baseline.r
@@ -36,9 +36,11 @@ base.d <- baseline %>%
 
 base.df <- baseline %>%
   select(`Av. Speed (mm/s)`, `Total Distance (mm)`, `Av. Accel (mm/s^2)`, `Mobility Rate (%)`, `Exploration Rate (%)`, `Total Distance (mm)`, `Time In Center (m:s)`, `Tot. Time Frozen (m:s)`, `Exploration Rate (%)`  )
+#gets all of our DVs that we are testing for into one data frame
 
 base.df.ID <- baseline %>%
   select(ID)
+#adds another column data frame with the IDs of each individual that corresponds to its numbered row
 
 base.df$`Time In Center (m:s)`<- (as.numeric(base.df$`Time In Center (m:s)`))/3600
 base.df$`Tot. Time Frozen (m:s)`<- (as.numeric(base.df$`Tot. Time Frozen (m:s)`))/3600
@@ -46,11 +48,14 @@ base.df$`Tot. Time Frozen (m:s)`<- (as.numeric(base.df$`Tot. Time Frozen (m:s)`)
 #Purpose of making base.df is solely for PCA analysis. Will give you eigenvalues based on where DVs load
 
 pca1 <- prcomp(base.df, center = TRUE, scale. = TRUE)
-summary(pca1) #gives summary of PCA with StDev and % of variance explained by PCAs
+summary(pca1) 
+#gives summary of PCA with StDev and % of variance explained by PCAs
 #most variance explained by PCA1, with PCA2 and PCA3 explaining good portion of variance as well
+
 str(pca1)
 #pca1 $Rotation gives us our Factor Loading values
 #pca1 $x gives us our REGR scores
+
 ggbiplot(pca1)
 # ggbiplot(pca1, labels=rownames(base.df.ID$ID)) 
 
@@ -59,6 +64,8 @@ treatment <- c(rep("Colonized", 1),rep("Depleted", 2), rep("Colonized", 1), rep(
                rep("Colonized", 1), rep("Depleted", 1), rep("Colonized", 1), rep("Depleted", 1),
                rep("Colonized", 1), rep("Depleted", 1), rep("Colonized", 1), rep("Depleted", 1), rep("Colonized", 1), rep("Colonized", 1),
                rep("Depleted", 1))
+#creates our values of treatment for each individual 
+
 ggbiplot(pca1, ellipse=TRUE, obs.scale = 1, var.scale = 1, groups = treatment)
 # can include ID names instead of just having the dots: labels = (baseline$ID)
 # can remove arrows by adding in var.axes=FALSE command
@@ -80,14 +87,16 @@ for (i in 1:nrow(baseline)) {
     }
 }
 baseline$cat <- factor(baseline$cat)
-bartlett.test(baseline$`Total Distance (mm)`~baseline$cat)
 
+#bartlett.test(baseline$`Total Distance (mm)`~baseline$cat)
+#place to run bartlett test
 
-# base.anova <- baseline %>%
-#   select(`Av. Speed (mm/s)`, `Total Distance (mm)`, `Av. Accel (mm/s^2)`, `Mobility Rate (%)`, `Exploration Rate (%)`, `Total Distance (mm)`, `Time In Center (m:s)`, `Tot. Time Frozen (m:s)`, `Exploration Rate (%)`, cat )
-#   
-# base.anova$`Time In Center (m:s)`<- (as.numeric(base.anova$`Time In Center (m:s)`))/3600
-# base.anova$`Tot. Time Frozen (m:s)`<- (as.numeric(base.anova$`Tot. Time Frozen (m:s)`))/3600
+base.anova <- baseline %>%
+  select(`Av. Speed (mm/s)`, `Total Distance (mm)`, `Av. Accel (mm/s^2)`, `Mobility Rate (%)`, `Exploration Rate (%)`, `Total Distance (mm)`, `Time In Center (m:s)`, `Tot. Time Frozen (m:s)`, `Exploration Rate (%)`, cat )
+
+base.anova$`Time In Center (m:s)`<- (as.numeric(base.anova$`Time In Center (m:s)`))/3600
+base.anova$`Tot. Time Frozen (m:s)`<- (as.numeric(base.anova$`Tot. Time Frozen (m:s)`))/3600
+#need to get hms into minutes again
 
 pca.value <- pca1$x  
 #These appear to be our REGR scores for PCA 1
@@ -104,3 +113,12 @@ summary(PCA.aov2)
 
 PCA.aov3 <- aov(pca.value...3.~baseline.cat, data = pca.value3)
 summary(PCA.aov3)
+
+baselinePCA1_glmm <- glm(pca.value...1.~baseline.cat, data = pca.value1)
+Anova(baselinePCA1_glmm)
+
+baselinePCA2_glmm <- glm(pca.value...2.~baseline.cat, data = pca.value2)
+Anova(baselinePCA2_glmm)
+
+baselinePCA3_glmm <- glm(pca.value...3.~baseline.cat, data = pca.value3)
+Anova(baselinePCA3_glmm)
